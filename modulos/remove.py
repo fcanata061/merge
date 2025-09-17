68d0f79c83502decf7c6c8edffc67a3bb0195b9c
fcanata061 <fcanata61@gmail.com>
2025-09-17 19:00:09 -0300

    Update remove.py

--- modulos/remove.py
+++ modulos/remove.py
@@ -1,13 +1,16 @@
 import os
 from .config import cfg
+from .logs import log
 
 def remove_package(package_name):
     install_path = cfg.get("global", "install_path")
     package_path = os.path.join(install_path, package_name)
     if os.path.exists(package_path):
         os.rmdir(package_path)
+        log(f"Pacote '{package_name}' removido.")
         print(f"Pacote '{package_name}' removido.")
         return True
     else:
         print(f"Pacote '{package_name}' não está instalado.")
+        log(f"Falha ao remover '{package_name}': não instalado.")
         return False
