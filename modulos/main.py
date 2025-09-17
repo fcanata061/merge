ed368096b1964b207308fb08dd49b50b7e2a70e1
fcanata061 <fcanata61@gmail.com>
2025-09-17 18:53:19 -0300

    Create main.py

---
+++ modulos/main.py
@@ -0,0 +1,34 @@
+import sys
+from .install import install_package
+from .remove import remove_package
+from .repository import list_packages
+
+def print_help():
+    print("""
+merge - Gerenciador de pacotes simples
+Comandos:
+    install <pacote>   - Instala um pacote
+    remove <pacote>    - Remove um pacote
+    list               - Lista pacotes disponíveis
+    help               - Mostra esta ajuda
+""")
+
+def main():
+    if len(sys.argv) < 2:
+        print_help()
+        return
+
+    cmd = sys.argv[1]
+
+    if cmd == "install" and len(sys.argv) == 3:
+        install_package(sys.argv[2])
+    elif cmd == "remove" and len(sys.argv) == 3:
+        remove_package(sys.argv[2])
+    elif cmd == "list":
+        for pkg in list_packages():
+            print(pkg)
+    else:
+        print_help()
+
+if __name__ == "__main__":
+    main()
